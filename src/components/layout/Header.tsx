0337db020049338dded7e55cf8bd93eb1b765a66
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-11-21 15:09:20 -0600

    chore(header): remove unnecessary border-border class

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -16,7 +16,7 @@ export const Header = () => {
     });
 
   return (
-    <header className="fixed top-0 z-50 w-full border-border border-b shadow-sm blur-ms">
+    <header className="fixed top-0 z-50 w-full border border-b shadow-sm blur-ms">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <Link to="/">
