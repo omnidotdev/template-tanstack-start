d7c1f7d2462700e26589029c3c4e40d753a82bc1
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-11-24 22:02:44 -0600

    fix(pricing): fix typo

--- src/routes/_auth/profile.tsx
+++ src/routes/_auth/profile.tsx
@@ -74,7 +74,7 @@ const fetchSubscriptions = createServerFn()
     return subscriptions.data.map((sub) => ({
       id: sub.id,
       // NB: type cast is required here so that the server function knows this is serializable.
-      // We do not expand customer, this this will always return the customer ID, thus this type casting is safe
+      // We do not expand customer, so this will always return the customer ID, thus this type casting is safe
       customerId: sub.customer as string,
       price: sub.items.data[0].price,
     }));
